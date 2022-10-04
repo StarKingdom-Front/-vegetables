af930086f5e5c97bf9dcac606f01204744d393ff
Aleksey <glinkirk@gmail.com>
2022-10-04 20:23:33 +0300

    Формы настроены

--- js/script.js
+++ js/script.js
@@ -226,5 +226,82 @@ function closeModal() {
         ".menu .container"
     ).render();
 
+    ////forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        succes: 'Спасибо, скоро свяжемся',
+        failure: 'Что-то пошло не так....'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage); 
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            ////присылаю данные в formdata
+
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+            // const formData = new FormData(form);
+
+            // request.send(formData);
+
+            // request.addEventListener('load', () => {
+            //     if (request.status === 200) {
+            //         console.log(request.response);
+            //         statusMessage.textContent = message.loading;
+            //         form.reset();
+            //         setTimeout(() => {
+            //             statusMessage.remove();
+            //         }, 2000);
+            //     } else {
+            //         statusMessage.textContent = message.failure;
+            //     }
+            // });
+
+            ///формат для json
+
+             request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.loading;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
+
+    
+
 });
 
